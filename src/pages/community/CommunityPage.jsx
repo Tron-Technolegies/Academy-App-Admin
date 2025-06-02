57267b5e22f92b1b18a780516b74b0b5961ea340
fathimahanaan <hanaanfh764@icloud.com>
2025-06-02 12:02:50 +0100

    community

--- src/pages/community/CommunityPage.jsx
+++ src/pages/community/CommunityPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CommunityList from "../../components/community/CommunityList";
-import CommunityHeader from "../../components/community/communityHeader";
+import CommunityHeader from "../../components/community/CommunityHeader";
 import CommunityNavLink from "../../components/community/CommunityNavLink";
 
 const CommunityPage = () => {
